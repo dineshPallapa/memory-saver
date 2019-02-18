9c3df6ae064acd171dd65ba07f95f482174dbb6e
dineshPallapa <dinesh@enroco.com>
2019-02-18 09:53:37 +0530

    Added ablity to search word with a hash

---
+++ convert-phone-words.rb
@@ -0,0 +1,18 @@
+require './available_words.rb'
+# Dictionary::WORDHASH
+def match_key_to_number
+  {
+    2 => ['a', 'b', 'c'],
+    3 => ['d', 'e', 'f'],
+    4 => ['g', 'h', 'i'],
+    5 => ['j', 'k', 'l'],
+    6 => ['m', 'n', 'o'],
+    7 => ['p', 'q', 'r', 's'],
+    8 => ['t', 'u', 'v'],
+    9 => ['w', 'x', 'y' 'z']
+  }
+end
+
+def convert_phone_to_word(phone)
+
+end
