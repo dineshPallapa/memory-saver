9ea4cc09a0f6e7b7e7ff8f9db41c5df3c17e5ac4
dineshPallapa <dinesh@enroco.com>
2019-03-03 14:47:13 +0530

    Added a logic for converting phone number to array of words

--- convert-phone-words.rb
+++ convert-phone-words.rb
@@ -1,5 +1,8 @@
+start = Time.now
 require './available_words.rb'
-# Dictionary::WORDHASH
+finish = Time.now
+p "time taken to load Dictionary hash #{finish - start}"
+
 def match_key_to_number
   {
     2 => ['a', 'b', 'c'],
@@ -13,6 +16,66 @@ def match_key_to_number
   }
 end
 
-def convert_phone_to_word(phone)
-
+def is_word_in_dictionary(word)
+  Dictionary::WORDHASH[word.upcase].nil? ? false : true
 end
+
+def is_validate_phone(phone_number)
+  if(phone_number.include?("0") || phone_number.include?("1"))
+    false
+  else
+    true
+  end
+end
+
+def convert_phone_to_word(phone_number)
+  words_combinations = nil
+
+  phone_number.to_s.split("").each do |number|
+    get_key = match_key_to_number[number.to_i]
+    if !words_combinations.nil?
+      words_combinations = words_combinations.product(get_key)
+    else
+      words_combinations = get_key
+    end
+  end
+
+  all_words = words_combinations.map { |word| word.flatten().join}
+  all_words.select! { |w| is_word_in_dictionary(w) }
+end
+
+def phone_to_words(phone)
+
+  if(is_validate_phone(phone))
+    possible_match = convert_phone_to_word(phone)
+    temp_phone = phone.clone
+    first_word = ''
+    last_word = ''
+
+    possible_phone_combinations = []
+
+    while temp_phone.size > 3 do
+      first_word += temp_phone.slice!(0)
+      last_word = temp_phone
+      if (first_word.size >= 3)
+        possible_phone_combinations << [convert_phone_to_word(first_word), convert_phone_to_word(last_word)]
+      end
+    end
+
+  else
+    raise "Phone number didn't accept '0' or '1' "
+  end
+
+  possible_phone_combinations << possible_match
+  return possible_phone_combinations.map{|a| a.reject(&:empty?)}
+end
+
+start = Time.now
+p phone_to_words("6686787825")
+finish = Time.now
+p "time taken to run 'phone_to_words(6686787825)' #{finish - start}"
+
+start = Time.now
+p phone_to_words("2282668687")
+finish = Time.now
+p "time taken to run 'phone_to_words(2282668687)' #{finish - start}"
